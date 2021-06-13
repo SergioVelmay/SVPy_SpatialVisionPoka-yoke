d2c4f4539ffaec2be753fc621c56ce5215922347
Sergio Velmay <sergiovelmay@gmail.com>
2021-06-13 18:23:54 +0200

    Prediction classes created.

---
+++ SVPy/classes_prediction.py
@@ -0,0 +1,29 @@
+class Classification:
+
+    def __init__(self, label, probability):
+        self.Label = label
+        self.Probability = probability
+
+    def __str__(self):
+        words = str.upper(self.Label).split('.')
+        if (self.Label.startswith('Step') or self.Label.startswith('Part')):
+            words[0] = words[0][:4] + ' #' + words[0][4:]
+        return ' - '.join(tuple(words)) + '   ( {:.1f}'.format(self.Probability) + '% )'
+
+class Boundary:
+
+    def __init__(self, x, y, w, h):
+        self.Left = x
+        self.Top = y
+        self.Width = w
+        self.Height = h
+
+    def __str__(self):
+        return 'x:{:.2f} y:{:.2f} w:{:.2f} h:{:.2f}'.format(
+            self.Left, self.Top, self.Width, self.Height)
+
+class Detection(Classification):
+
+    def __init__(self, label, probability, x, y, w, h):
+        Classification.__init__(self, label, probability)
+        self.Box = Boundary(x, y, w, h)
