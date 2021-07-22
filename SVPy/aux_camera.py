28bbbabb1cf4bd60a65efd9307d4d980ea56df8e
Sergio Velmay <sergiovelmay@gmail.com>
2021-07-22 20:59:31 +0200

    Auxiliary camera added.

---
+++ SVPy/aux_camera.py
@@ -0,0 +1,29 @@
+import depthai as dai
+import threading
+
+class Camera:
+    def __init__(self) -> None:
+        self.NAME = 'RGB'
+        self.Pipeline = dai.Pipeline()
+        self.Create()
+        print('[ SVPy ] Depth AI RGB Camera created')
+        self.Frame = None
+        threading.Thread(target=self.Capture, daemon=True).start()
+        print('[ SVPy ] Depth AI RGB Camera capturing')
+
+    def Create(self):
+        self.RGB_Camera = self.Pipeline.createColorCamera()
+        self.RGB_Camera.setPreviewSize(640, 480)
+        self.RGB_Camera.setInterleaved(False)
+        self.RGB_Camera.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+        self.RGB_XLinkOut = self.Pipeline.createXLinkOut()
+        self.RGB_XLinkOut.setStreamName(self.NAME)
+        self.RGB_Camera.preview.link(self.RGB_XLinkOut.input)
+
+    def Capture(self):
+        with dai.Device() as device:
+            device.startPipeline(self.Pipeline)
+            self.RGB_Queue = device.getOutputQueue(name=self.NAME, maxSize=1, blocking=False)
+            while True:
+                self.RGB_Data = self.RGB_Queue.get()
+                self.Frame = self.RGB_Data.getCvFrame()
